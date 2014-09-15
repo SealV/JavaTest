7e918823b9c1a7bff9a12ddffb01487f3a9d03d7
ONE <56623345@qq.com>
2014-09-15 17:43:50 +0800

    第三次修改 添加一个类

---
+++ src/Clone.java
@@ -0,0 +1,18 @@
+// // // // // // // // // // // // // // // // // // // // // // // // // // //
+// Copyright ownership belongs to SINOSOFT CO.,LTD.shall not be reproduced ,  //
+// copied,or used in other ways without permission.Otherwise SINOSOFT CO.,LTD.//
+// will have the right to pursue legal responsibilities.                      //
+//                                                                            //
+// Copyright &copy; 2012 SINOSOFT CO., LTD. All rights reserved.              //
+// // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+/**
+ * User: Administrator
+ * Date: 2014/9/15
+ * Time: 17:42
+ */
+public class Clone {
+    public void test(){
+        System.out.println("Clone");
+    }
+}
