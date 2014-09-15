6ae445c4dd3227351ec4ebb7440987e35032e99a
ONE <56623345@qq.com>
2014-09-15 17:26:56 +0800

    下载后第一次修改

--- src/GitTest.java
+++ src/GitTest.java
@@ -14,6 +14,6 @@
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("this is a git!");
+        System.out.println("this is a git! change!~~~ONE");
     }
 }
