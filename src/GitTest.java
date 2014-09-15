2e61698649565c7d75e374f22bedf47b5902eb54
ONE <56623345@qq.com>
2014-09-15 11:25:35 +0800

    首次提交

---
+++ src/GitTest.java
@@ -0,0 +1,19 @@
+// // // // // // // // // // // // // // // // // // // // // // // // // // //
+// Copyright ownership belongs to SINOSOFT CO.,LTD.shall not be reproduced ,  //
+// copied,or used in other ways without permission.Otherwise SINOSOFT CO.,LTD.//
+// will have the right to pursue legal responsibilities.                      //
+//                                                                            //
+// Copyright &copy; 2012 SINOSOFT CO., LTD. All rights reserved.              //
+// // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+/**
+ * User: Administrator
+ * Date: 2014/9/15
+ * Time: 11:23
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("this is a git!");
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,25 @@
+// // // // // // // // // // // // // // // // // // // // // // // // // // //
+// Copyright ownership belongs to SINOSOFT CO.,LTD.shall not be reproduced ,  //
+// copied,or used in other ways without permission.Otherwise SINOSOFT CO.,LTD.//
+// will have the right to pursue legal responsibilities.                      //
+//                                                                            //
+// Copyright &copy; 2012 SINOSOFT CO., LTD. All rights reserved.              //
+// // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+/**
+ * User: Administrator
+ * Date: 2014/8/21
+ * Time: 11:02
+ */
+import java.util.Date;
+public  class Test extends Date{
+	public static void main(String[] args) {
+		new Test().test();
+	}
+
+	public void test(){
+		System.out.println(super.getClass().getCanonicalName());
+	}
+}
+
+
